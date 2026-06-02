502fef81498b2c8c83030b89a42a1b3a4ce8e0ca
もいずる <momizibe@outlook.jp>
2026-06-03 07:14:55 +0900

    Update Footer.tsx

--- src/components/sections/Footer/Footer.tsx
+++ src/components/sections/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="w-full fixed bottom-0 left-0 text-right font-bold pointer-events-none">
+        <footer className="w-full fixed bottom-17 md:bottom-0 left-0 text-right font-bold pointer-events-none">
             <a target="_blank" href="https://moiz.lu/contact" className="pointer-events-auto">
                 お問い合わせはこちら(別サイトに飛びます) ↗
             </a>
