7a03740ff2241a72e45b433b9127f86bb6ced587
もいずる <momizibe@outlook.jp>
2026-06-03 08:17:21 +0900

    GitHubへのリンクを追加

--- src/components/sections/Footer/Footer.tsx
+++ src/components/sections/Footer/Footer.tsx
@@ -1,6 +1,15 @@
+import GitHubLight from "@/assets/icons/light/github.svg";
+import GitHubDark from "@/assets/icons/dark/github.svg";
+
+import Icon from "@/components/ui/Icon";
+
 const Footer = () => {
     return (
-        <footer className="w-full fixed z-1000 max-md:top-5 md:bottom-0 -left-1 text-xs text-right font-bold pointer-events-none">
+        <footer className="w-full fixed z-1000 px-4 max-md:top-5 md:bottom-1 left-0 text-xs text-right font-bold pointer-events-none flex justify-between items-center max-md:flex-row-reverse">
+            <a target="_blank" title="github" href="https://moiz.lu/gh/shimane-guide" className="max-md:absolute -bottom-15 left-1 pointer-events-auto">
+                <Icon lightSrc={GitHubLight} darkSrc={GitHubDark} alt="github logo" size={30} />
+            </a>
+
             <a target="_blank" href="https://moiz.lu/contact" className="pointer-events-auto">
                 お問い合わせはこちら(別サイトに飛びます) ↗
             </a>
