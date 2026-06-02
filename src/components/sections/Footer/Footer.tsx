b2ca1fdf9d8eec20f93dd2a3b8ac15ddac5e07f8
もいずる <momizibe@outlook.jp>
2026-06-03 07:17:43 +0900

    デザインを調整

--- src/components/sections/Footer/Footer.tsx
+++ src/components/sections/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="w-full fixed bottom-17 md:bottom-0 left-0 text-right font-bold pointer-events-none">
+        <footer className="w-full fixed z-1000 max-md:top-5 md:bottom-0 -left-1 text-xs text-right font-bold pointer-events-none">
             <a target="_blank" href="https://moiz.lu/contact" className="pointer-events-auto">
                 お問い合わせはこちら(別サイトに飛びます) ↗
             </a>
